0c3b672f130eb30440355f191f3c31d54917a8b0
Frost-704 <boolean.pg.me@gmail.com>
2024-04-13 01:58:34 +0300

    feat: implement memoize solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -127,8 +127,15 @@ function getPolynom(...pows) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const memory = {};
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (!(key in memory)) {
+      memory[key] = func(...args);
+    }
+    return memory[key];
+  };
 }
 
 /**
