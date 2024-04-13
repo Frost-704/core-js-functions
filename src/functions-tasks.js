c3cb20ae804b9947007f2bbf9bc672a97f95d16e
Frost-704 <boolean.pg.me@gmail.com>
2024-04-13 16:25:48 +0300

    feat: implement logger solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -190,8 +190,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function getLog(...args) {
+    const argsStr = args.map((arg) => JSON.stringify(arg)).join(',');
+    logFunc(`${func.name}(${argsStr}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argsStr}) ends`);
+    return result;
+  };
 }
 
 /**
